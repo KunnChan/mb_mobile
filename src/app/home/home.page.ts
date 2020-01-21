64f85e1cb63bf854222e88fc4347212e17a85e31
kunnchan <barbiq36@gmail.com>
2020-01-21 23:47:12 +0630

    add home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,9 +12,56 @@ export class HomePage implements OnInit {
     speed: 100
   };
 
+  listCardsAdventure = [
+    {
+      title: "Aaaaaa",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Bbbbbb",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Cccccc",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Dddddd",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Eeeeee",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Ffffff",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Gggggg",
+      imageUrl: "../../assets/album.jpg"
+    },
+    {
+      title: "Hhhhhh",
+      imageUrl: "../../assets/album.jpg"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  logScrollStart(){
+    console.log("item ");
+  }
+
+  logScrolling(event){
+    console.log("item ", event);
+  }
+
+  logScrollEnd(){
+    console.log("item ");
   }
 
 }
